fc2fb9a9a6632896100a176b20e28a051499ed30
Ben Sorahan <ben.sorahan@gmail.com>
2021-10-03 21:11:20 +1100

    Change example docker stack name

--- examples/docker-stack-prometheus/run.sh
+++ examples/docker-stack-prometheus/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-stack_name="process"
+stack_name="my-monitored-app"
 mvn package -Dmaven.test.skip=true -f "../monitored-app/pom.xml"
-mkdir -p "target" && cp ../monitored-app/target/*.jar ./target
-docker build --pull --rm -f Dockerfile -t camundamonitoring .
+mkdir -p "target" && rm -f ./target/* && cp ../monitored-app/target/*.jar ./target
+docker build --pull --rm -f Dockerfile -t my-monitored-app .
 docker stack deploy $stack_name --compose-file docker-compose.yml
 docker stack services -q $stack_name \
   |while read service; do
